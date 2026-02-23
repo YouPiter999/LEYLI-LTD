6d2d85be694a9ef7dd786681d219f607c5612505
Sergey <-email@github.com>
2026-02-23 23:44:12 +0200

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/LEYLI-LTD/',
   plugins: [react()],
   server: {
     host: true,
